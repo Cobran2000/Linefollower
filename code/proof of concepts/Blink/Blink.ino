f0c3a5e323f48f644334a655d986ca36784ea1f3
Cobran2000 <104622841+Cobran2000@users.noreply.github.com>
2025-11-05 16:49:05 +0100

    Add files via upload

---
+++ code/proof of concepts/Blink/Blink.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //Blink in de setup (10 keren)
+  for (int i; i <= 10; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(250);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(250);
+  }
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
